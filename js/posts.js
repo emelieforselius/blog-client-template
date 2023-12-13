31d90bff75e3abe4bae4dd5e80afd9ea508e0888
Emelie Forselius <emelieforselius@outlook.com>
2023-12-13 16:41:31 +0100

    enkel styling

--- js/posts.js
+++ js/posts.js
@@ -8,13 +8,14 @@ async function fetchAllPosts() {
         for (let post of posts) {
 
             postListHTML += `
+            <div class="blog-post">
             <h2>${post.title}</h2>
             <p>
-            ${post.author} | ${new Date(post.date).toLocaleString()}
+            <em>${post.author}</em> | <em>${new Date(post.date).toLocaleString()}</em>
             </p>
-            <p>tags: ${post.tags ? post.tags.join(', ') : ''}</p>
-            <p>${post.content ? post.content.slice(0, 100) : ''}</p>
-            <a href="post.html?id=${post._id}">...read more</a>
+            <p><strong>tags:</strong> ${post.tags ? post.tags.join(', ') : ''}</p>
+            <p>${post.content ? post.content.slice(0, 100) : ''} <a href="post.html?id=${post._id}">...read more</a></p>
+            </div>
             `;
         }
 
